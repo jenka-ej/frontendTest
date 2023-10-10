cb6509ec61926122867c9c9c6a28fa8aef063feb
Evgeny Filippov <85373955+jenka-ej@users.noreply.github.com>
2023-10-10 20:18:08 +0300

    Update index.js

--- task_1/index.js
+++ task_1/index.js
@@ -3,6 +3,32 @@ import {encoded, translations} from './data.js'
 console.log("Let's rock")
 console.log(encoded, translations)
 
+const decoded = () => {
+    const exceptions = ['groupId', 'service', 'formatSize', 'ca'];
+    const ids = [];
+    const translate = encoded.map((item) => {
+        const keys = Object.keys(item);
+        const newItem = keys.reduce((acc, key) => {
+            const value = item[key];
+            if (key.includes('Id') && !exceptions.includes(key)) {
+                ids.push(value);
+                const translate = translations[value];
+                if (!translate && translate !== '') {
+                    acc[key] = value;
+                    return acc;
+                }
+                acc[key] = translate;
+                return acc;
+            }
+            acc[key] = value;
+            return acc;
+        }, {});
+        return newItem;
+    });
+    console.log(translate);
+    const decodeKeys = Object.keys(translations);
+    const uniqueIds = ids.filter((id) => !decodeKeys.includes(id));
+    return [...new Set(uniqueIds)];
+};
 
-
-// console.log(decoded)
+console.log(decoded());
