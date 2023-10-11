d87c1a0a9ed7327bb32e461a1625dd18a0dcb91a
Evgeniy Filippov <Zhekafilipp@mail.ru>
2023-10-11 22:19:04 +0300

    finished second task ^^

--- task_2/src/main.js
+++ task_2/src/main.js
@@ -3,38 +3,37 @@ const finalCurrency = document.getElementById("finalCurrency");
 const amountInput = document.getElementById("amountInput");
 const amountOutput = document.getElementById("amountOutput");
 
-const directConvert = () => {
+const directConvert = async () => {
   const baseCurrencyValue = baseCurrency.value;
   const finalCurrencyValue = finalCurrency.value;
   const amountInputValue = amountInput.value;
 
-  fetch(`https://v6.exchangerate-api.com/v6/YOUR_API_KEY/latest/${baseCurrencyValue}`)
-    .then((response) => response.json())
-    .then((data) => {
-      const exchangeRate = data.conversion_rates[finalCurrencyValue];
-      const convertedAmount = amountInputValue * exchangeRate;
-      amountOutput.value = Number(convertedAmount.toFixed(1));
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  
+  try {
+    const response = await fetch(`https://open.er-api.com/v6/latest/${baseCurrencyValue}`);
+    const data = await response.json();
+    const exchangeRate = data.conversion_rates[finalCurrencyValue];
+    const convertedAmount = amountInputValue * exchangeRate;
+    amountOutput.value = Number(convertedAmount.toFixed(1));
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-const reverseConvert = () => {
+const reverseConvert = async () => {
   const baseCurrencyValue = baseCurrency.value;
   const finalCurrencyValue = finalCurrency.value;
   const amountOutputValue = amountOutput.value;
-
-  fetch(`https://v6.exchangerate-api.com/v6/YOUR_API_KEY/latest/${finalCurrencyValue}`)
-    .then((response) => response.json())
-    .then((data) => {
-      const exchangeRate = data.conversion_rates[baseCurrencyValue];
-      const convertedAmount = amountOutputValue * exchangeRate;
-      amountInput.value = Number(convertedAmount.toFixed(1));
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  
+  try {
+    const response = await fetch(`https://open.er-api.com/v6/latest/${finalCurrencyValue}`);
+    const data = await response.json();
+    const exchangeRate = data.conversion_rates[baseCurrencyValue];
+    const convertedAmount = amountOutputValue * exchangeRate;
+    amountInput.value = Number(convertedAmount.toFixed(1));
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 baseCurrency.addEventListener("change", directConvert);
