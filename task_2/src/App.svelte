9e0ec67f84f305109d96580f45e7ff56aca63d25
Evgeniy Filippov <Zhekafilipp@mail.ru>
2023-10-12 01:41:35 +0300

    rebuild with Svelte

--- task_2/src/App.svelte
+++ task_2/src/App.svelte
@@ -1,47 +1,86 @@
-<script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+<script>
+  let baseCurrency = 'RUB';
+  let finalCurrency = 'RUB';
+  let amountInput = 0;
+  let amountOutput = 0;
+
+  const apiKey = import.meta.env.VITE_API_KEY;
+
+  function convert(type) {
+    let url;
+    if (type === 'direct') {
+      url = `https://v6.exchangerate-api.com/v6/${apiKey}/pair/${baseCurrency}/${finalCurrency}/${amountInput}`;
+    } else {
+      url = `https://v6.exchangerate-api.com/v6/${apiKey}/pair/${finalCurrency}/${baseCurrency}/${amountOutput}`;
+    }
+    
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        const result = data.conversion_result;
+        if (type === 'direct') {
+	  amountOutput = Number(result.toFixed(1));
+        } else {
+	  amountInput = Number(result.toFixed(1));
+        }
+      })
+      .catch((err) => console.log(err));
+  }  
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+  <h1>Сервис для конвертации валют</h1>
+  
+  <div class="form-group">
+    <label for="baseCurrency">Исходная валюта:</label>
+    <select id="baseCurrency" bind:value={baseCurrency}>
+      <option value="RUB">RUB</option>
+      <option value="USD">USD</option>
+      <option value="EUR">EUR</option>
+      <option value="GBP">GBP</option>
+      <option value="KZT">KZT</option>
+      <option value="ETB">ETB</option>
+    </select>
   </div>
-  <h1>Vite + Svelte</h1>
 
-  <div class="card">
-    <Counter />
+  <div class="form-group">
+    <label for="finalCurrency">Итоговая валюта:</label>
+    <select id="finalCurrency" bind:value={finalCurrency}>
+      <option value="RUB">RUB</option>
+      <option value="USD">USD</option>
+      <option value="EUR">EUR</option>
+      <option value="GBP">GBP</option>
+      <option value="KZT">KZT</option>
+      <option value="ETB">ETB</option>
+    </select>
   </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  
+  <div class="form-group">
+    <label for="amountInput">Сумма конвертации:</label>
+    <input type="number" bind:value={amountInput} on:change={() => convert('direct')} id="amountInput" min="0" step="0.1" />
+  </div>
+ 
+  <div class="form-group">
+    <label for="amountOutput">Результат конвертации:</label>
+    <input type="number" bind:value={amountOutput} on:change={() => convert('reverse')} id="amountOutput" min="0" step="0.1" />
+  </div>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+  .form-group {
+    margin-bottom: 20px;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+
+  .form-group label {
+    display: block;
+    font-weight: bold;
+    margin-bottom: 10px;
   }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+
+  .form-group input {
+    width: 20%;
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
   }
 </style>
