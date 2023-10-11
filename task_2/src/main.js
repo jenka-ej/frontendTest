e369249d272c8604fffc6846888cbbcb3e0a8545
Evgeniy Filippov <Zhekafilipp@mail.ru>
2023-10-11 14:49:19 +0300

    finished second task^^

--- task_2/src/main.js
+++ task_2/src/main.js
@@ -3,7 +3,7 @@ const finalCurrency = document.getElementById("finalCurrency");
 const amountInput = document.getElementById("amountInput");
 const amountOutput = document.getElementById("amountOutput");
 
-function directConvert() {
+const directConvert = () => {
   const baseCurrencyValue = baseCurrency.value;
   const finalCurrencyValue = finalCurrency.value;
   const amountInputValue = amountInput.value;
@@ -13,14 +13,14 @@ function directConvert() {
     .then((data) => {
       const exchangeRate = data.conversion_rates[finalCurrencyValue];
       const convertedAmount = amountInputValue * exchangeRate;
-      amountOutput.value = Number(convertedAmount.toFixed(2));
+      amountOutput.value = Number(convertedAmount.toFixed(1));
     })
     .catch((err) => {
       console.log(err);
     });
 }
 
-function reverseConvert() {
+const reverseConvert = () => {
   const baseCurrencyValue = baseCurrency.value;
   const finalCurrencyValue = finalCurrency.value;
   const amountOutputValue = amountOutput.value;
@@ -30,14 +30,14 @@ function reverseConvert() {
     .then((data) => {
       const exchangeRate = data.conversion_rates[baseCurrencyValue];
       const convertedAmount = amountOutputValue * exchangeRate;
-      amountInput.value = Number(convertedAmount.toFixed(2));
+      amountInput.value = Number(convertedAmount.toFixed(1));
     })
     .catch((err) => {
       console.log(err);
     });
 }
 
-baseCurrency.addEventListener("change", convertCurrency);
-finalCurrency.addEventListener("change", convertCurrency);
+baseCurrency.addEventListener("change", directConvert);
+finalCurrency.addEventListener("change", directConvert);
 amountInput.addEventListener("input", directConvert);
 amountOutput.addEventListener("input", reverseConvert);
